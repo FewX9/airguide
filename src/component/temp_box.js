78a3e9a290af33503c3ee6b99c82357780175808
FewX <garfield8283@gmail.com>
2023-10-28 18:04:11 +0700

    temp-box

--- src/component/temp_box.js
+++
@@ -1,7 +0,0 @@
-export default function TempBox({ children }) {
-  children = JSON.parse(children);
-  console.log(children.key1);
-  return (
-    <h1 className="text-2xl font-semibold">{children.key1}</h1>
-  )
-}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,14 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import TempBox from '@/component/temp_box'
+import TempBox from '@/components/temp_box'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const jsonData = { key1: 'value1', key2: 'value2' };
-  const jsonString = JSON.stringify(jsonData);
-
   return (
     <>
     <TempBox>
-      {jsonString}
+      
     </TempBox>
     </>
   )
