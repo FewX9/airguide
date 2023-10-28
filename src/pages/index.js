091b4a7dcea0cd1bfebbfde4dd5549e8da3407c2
leveliz <sindevilkung@gmail.com>
2023-10-28 18:44:53 +0700

    navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import TempBox from '@/components/temp_box'
 import DateWeather from '@/components/DateWeather'
+import Navbar from '@/components/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
+    <Navbar/>
     <TempBox>
     </TempBox>
     <DateWeather>
